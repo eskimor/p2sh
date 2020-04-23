8d2d681fe73c246a0d97597971b73c56ccde387f
Robert Klotzner <robert.klotzner@gmx.at>
2020-04-23 11:41:12 +0200

    Initial experiments.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,158 @@
+use async_std::{io, task};
+use futures::{future, prelude::*};
 use std::os::unix::io::RawFd;
+use libp2p::{
+    Multiaddr,
+    PeerId,
+    Swarm,
+    NetworkBehaviour,
+    identity,
+    floodsub::{self, Floodsub, FloodsubEvent},
+    mdns::{Mdns, MdnsEvent},
+    swarm::NetworkBehaviourEventProcess
+};
 use std::{
     path::{
         Path,
         PathBuf
     },
     process,
-    format
+    format,
+    error::Error,
+    task::{Context, Poll}
 };
 use termios::{Termios, ICANON, VTIME, VMIN};
 
-fn main() {
-    let raw_stdin = 0;
-    let mut termios = Termios::from_fd(raw_stdin).expect("Stdin is not a tty!");
-    println!("Your terminal is: {:?}", get_tty_path());
-    println!("Terminal settings: {:?}", termios);
-    if termios.c_lflag & ICANON != 0 {
-        println!("Terminal is canon!");
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    // Create a random PeerId
+    // TODO: Store key and only generate if not existing.
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    // Set up a an encrypted DNS-enabled TCP Transport over the Mplex and Yamux protocols
+    let transport = libp2p::build_development_transport(local_key)?;
+
+    // Create a Floodsub topic
+    let floodsub_topic = floodsub::Topic::new("chat");
+
+    // We create a custom network behaviour that combines floodsub and mDNS.
+    // In the future, we want to improve libp2p to make this easier to do.
+    // Use the derive to generate delegating NetworkBehaviour impl and require the
+    // NetworkBehaviourEventProcess implementations below.
+    #[derive(NetworkBehaviour)]
+    struct MyBehaviour {
+        floodsub: Floodsub,
+        mdns: Mdns,
+
+        // Struct fields which do not implement NetworkBehaviour need to be ignored
+        #[behaviour(ignore)]
+        #[allow(dead_code)]
+        ignored_member: bool,
     }
-    else {
-        println!("Terminal is not canon");
+
+    impl NetworkBehaviourEventProcess<FloodsubEvent> for MyBehaviour {
+        // Called when `floodsub` produces an event.
+        fn inject_event(&mut self, message: FloodsubEvent) {
+            if let FloodsubEvent::Message(message) = message {
+                println!("Received: '{:?}' from {:?}", String::from_utf8_lossy(&message.data), message.source);
+            }
+        }
     }
-    println!("VTIME: {}", termios.c_cc[VTIME]);
-    println!("VMIN: {}", termios.c_cc[VMIN]);
+
+    impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
+        // Called when `mdns` produces an event.
+        fn inject_event(&mut self, event: MdnsEvent) {
+            match event {
+                MdnsEvent::Discovered(list) =>
+                    for (peer, _) in list {
+                        self.floodsub.add_node_to_partial_view(peer);
+                    }
+                MdnsEvent::Expired(list) =>
+                    for (peer, _) in list {
+                        if !self.mdns.has_node(&peer) {
+                            self.floodsub.remove_node_from_partial_view(&peer);
+                        }
+                    }
+            }
+        }
+    }
+
+    // Create a Swarm to manage peers and events
+    let mut swarm = {
+        let mdns = Mdns::new()?;
+        let mut behaviour = MyBehaviour {
+            floodsub: Floodsub::new(local_peer_id.clone()),
+            mdns,
+            ignored_member: false,
+        };
+
+        behaviour.floodsub.subscribe(floodsub_topic.clone());
+        Swarm::new(transport, behaviour, local_peer_id)
+    };
+
+    // Reach out to another node if specified
+    if let Some(to_dial) = std::env::args().nth(1) {
+        let addr: Multiaddr = to_dial.parse()?;
+        Swarm::dial_addr(&mut swarm, addr)?;
+        println!("Dialed {:?}", to_dial)
+    }
+
+    // Read full lines from stdin
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+
+    // Listen on all interfaces and whatever port the OS assigns
+    Swarm::listen_on(&mut swarm, "/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // Kick it off
+    let mut listening = false;
+    task::block_on(future::poll_fn(move |cx: &mut Context| {
+        loop {
+            match stdin.try_poll_next_unpin(cx)? {
+                Poll::Ready(Some(line)) => swarm.floodsub.publish(floodsub_topic.clone(), line.as_bytes()),
+            Poll::Ready(None) => panic!("Stdin closed"),
+                Poll::Pending => break
+            }
+        }
+        loop {
+            match swarm.poll_next_unpin(cx) {
+                Poll::Ready(Some(event)) => println!("{:?}", event),
+                Poll::Ready(None) => return Poll::Ready(Ok(())),
+                Poll::Pending => {
+                    if !listening {
+                        for addr in Swarm::listeners(&swarm) {
+                            println!("Listening on {:?}", addr);
+                            listening = true;
+                        }
+                    }
+                    break
+                }
+            }
+        }
+        Poll::Pending
+    }))
 }
 
-fn get_tty_path() -> PathBuf {
-    let pid = process::id();
-    let path = format!("/proc/{}/fd/0", pid);
-    let path = Path::new(&path);
-    path.canonicalize().expect("Invalid path")
-}
+// fn main() {
+//     let raw_stdin = 0;
+//     let mut termios = Termios::from_fd(raw_stdin).expect("Stdin is not a tty!");
+//     println!("Your terminal is: {:?}", get_tty_path());
+//     println!("Terminal settings: {:?}", termios);
+//     if termios.c_lflag & ICANON != 0 {
+//         println!("Terminal is canon!");
+//     }
+//     else {
+//         println!("Terminal is not canon");
+//     }
+//     println!("VTIME: {}", termios.c_cc[VTIME]);
+//     println!("VMIN: {}", termios.c_cc[VMIN]);
+// }
+
+// fn get_tty_path() -> PathBuf {
+//     let pid = process::id();
+//     let path = format!("/proc/{}/fd/0", pid);
+//     let path = Path::new(&path);
+//     path.canonicalize().expect("Invalid path")
+// }
