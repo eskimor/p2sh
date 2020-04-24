d463825c85cbebb14070580cf0866ee47cbd1c71
Robert Klotzner <robert.klotzner@gmx.at>
2020-04-25 01:16:44 +0200

    Store and reload key.
    Also move to p2shd sub directory.

--- p2shd/src/main.rs
+++ p2shd/src/main.rs
@@ -9,26 +9,90 @@
 //!
 //! 4. Close with Ctrl-c.
 
+use anyhow::{Context as AnyhowContext, Result};
 use async_std::{io, task};
 use futures::prelude::*;
 use libp2p::kad::record::store::MemoryStore;
 use libp2p::kad::{record::Key, Kademlia, KademliaEvent, PutRecordOk, Quorum, Record};
 use libp2p::{
-    NetworkBehaviour,
-    PeerId,
-    Swarm,
-    build_development_transport,
-    identity,
+    build_development_transport, identity,
+    identity::ed25519,
     mdns::{Mdns, MdnsEvent},
-    swarm::NetworkBehaviourEventProcess
+    swarm::NetworkBehaviourEventProcess,
+    NetworkBehaviour, PeerId, Swarm,
 };
-use std::{error::Error, task::{Context, Poll}};
+use std::os::unix::fs::PermissionsExt;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    task::{Context, Poll},
+};
+use structopt::StructOpt;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[derive(StructOpt, Debug)]
+#[structopt(name = "p2shd")]
+struct Config {
+    /// The directory to read configuation files from. Defaults to the '.p2shd' directory in the
+    /// current directory.
+    #[structopt(long, parse(from_os_str), default_value = ".p2shd")]
+    config_dir: PathBuf,
+    #[structopt(long, parse(from_os_str), default_value = ".p2shd/node_key")]
+    key_file: PathBuf,
+}
+
+/// Load key from given file path (if present) or generate one and store it.
+///
+/// # Errors
+///
+/// 1. File cannot be read for other reasons than "Not Found".
+/// 3. Decoding of key fails.
+/// 2. File cannot be written.
+///
+/// If the given file exists but does not contain a valid Ed25519 key.
+pub fn gen_or_get_key(key_path: &Path) -> Result<ed25519::Keypair> {
+    may_read_key(key_path)?.map_or_else(|| gen_and_write_key(key_path), Ok)
+}
+
+/// Read key if key file exists.
+fn may_read_key(key_path: &Path) -> Result<Option<ed25519::Keypair>> {
+    // Use then, when it exists: https://doc.rust-lang.org/std/primitive.bool.html#method.then
+    if key_path.exists() {
+        let mut raw = fs::read(key_path)
+            .with_context(|| format!("Reading keyfile {} failed!", key_path.display()))?;
+
+        ed25519::Keypair::decode(&mut raw)
+            .map(Some)
+            .with_context(|| format!("Invalid keyfile {}!", key_path.display()))
+    } else {
+        Ok(None)
+    }
+}
+
+/// Generate a key and write it to the file given by path.
+fn gen_and_write_key(key_path: &Path) -> Result<ed25519::Keypair> {
+    let key = ed25519::Keypair::generate();
+    let encoded: &[u8] = &key.encode();
+    fs::write(key_path, encoded)
+        .with_context(|| format!("Writting keyfile {} failed!", key_path.display()))?;
+    // Only user should be able to read the file:
+    fs::set_permissions(key_path, PermissionsExt::from_mode(0o400))?;
+    Ok(key)
+}
+
+fn create_config_dir(config_path: &Path) -> Result<()> {
+    fs::create_dir_all(config_path)?;
+    fs::set_permissions(config_path, PermissionsExt::from_mode(0o700))?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
     env_logger::init();
 
+    let cfg = Config::from_args();
+    create_config_dir(&cfg.config_dir)?;
+
     // Create a random key for ourselves.
-    let local_key = identity::Keypair::generate_ed25519();
+    let local_key = identity::Keypair::Ed25519(gen_or_get_key(&cfg.key_file)?);
     let local_peer_id = PeerId::from(local_key.public());
     println!("Our peer id: {}", &local_peer_id);
 
@@ -39,7 +103,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     #[derive(NetworkBehaviour)]
     struct MyBehaviour {
         kademlia: Kademlia<MemoryStore>,
-        mdns: Mdns
+        mdns: Mdns,
     }
 
     impl NetworkBehaviourEventProcess<MdnsEvent> for MyBehaviour {
@@ -47,8 +111,12 @@ fn main() -> Result<(), Box<dyn Error>> {
         fn inject_event(&mut self, event: MdnsEvent) {
             if let MdnsEvent::Discovered(list) = event {
                 for (peer_id, multiaddr) in list {
-                    println!("MDNS, discovered peer {} with address {}!", peer_id, multiaddr);
+                    println!(
+                        "MDNS, discovered peer {} with address {}!",
+                        peer_id, multiaddr
+                    );
                     self.kademlia.add_address(&peer_id, multiaddr);
+                    self.kademlia.bootstrap();
                 }
             }
         }
@@ -66,18 +134,22 @@ fn main() -> Result<(), Box<dyn Error>> {
                             std::str::from_utf8(&value).unwrap(),
                         );
                     }
-                },
+                }
                 KademliaEvent::GetClosestPeersResult(peers_result) => {
                     println!("Found closest peers: {:?}", &peers_result);
                     for p in self.kademlia.kbuckets_entries() {
                         println!("Entry in our buckets: {:?}", p);
                     }
-                },
-                KademliaEvent::Discovered { peer_id, addresses, ty } => {
+                }
+                KademliaEvent::Discovered {
+                    peer_id,
+                    addresses,
+                    ty,
+                } => {
                     println!("Discovered peer: {}", peer_id);
                     println!("Addresses of that peer: {:?}", addresses);
                     println!("Connection status: {:?}", ty);
-                },
+                }
                 KademliaEvent::GetRecordResult(Err(err)) => {
                     eprintln!("Failed to get record: {:?}", err);
                 }
@@ -118,7 +190,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             match stdin.try_poll_next_unpin(cx)? {
                 Poll::Ready(Some(line)) => handle_input_line(&mut swarm.kademlia, line),
                 Poll::Ready(None) => panic!("Stdin closed"),
-                Poll::Pending => break
+                Poll::Pending => break,
             }
         }
         loop {
@@ -132,7 +204,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                             listening = true;
                         }
                     }
-                    break
+                    break;
                 }
             }
         }
